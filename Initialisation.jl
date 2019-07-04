59f58cc98c9958342e25196ec3aaea4c0f67e60f
AnneLew <45165523+AnneLew@users.noreply.github.com>
2019-07-04 16:37:02 +0200

    Update Initialisation.jl

--- Initialisation.jl
+++ Initialisation.jl
@@ -6,7 +6,7 @@ Initialisation of Climate, Daylength, Irradiance, Light
 # Forsythe, William C., Edward J. Rykiel Jr., Randal S. Stahl, Hsin-i Wu and Robert M. Schoolfield, 1995.
 # A model comparison for daylength as a function of latitude and day of the year. Ecological Modeling 80:87-95.
 
-function daylength(lat, doy)
+function daylength(lat::Float64, doy::Float64)
 #	if (class(doy) == 'Date' | class(doy) == 'character')
 #		doy = as.character(doy)
 #		doy = as.numeric(format(as.Date(doy), "%j"))
@@ -15,23 +15,23 @@ function daylength(lat, doy)
 #	end
 #	lat[lat > 90 | lat < -90] = NA
 	P::Float64 = asin(0.39795 * cos(0.2163108 + 2 * atan(0.9671396 * tan(0.00860*(doy-186)))))
-	a =  (sin(0.8333 * pi/180) + sin(lat * pi/180) * sin(P)) / (cos(lat * pi/180) * cos(P))
+	a::Float64 =  (sin(0.8333 * pi/180) + sin(lat * pi/180) * sin(P)) / (cos(lat * pi/180) * cos(P))
 #	a = pmin(pmax(a, -1), 1)
-	DL = 24 - (24/pi) * acos(a)
+	DL::Float64 = 24 - (24/pi) * acos(a)
 	return(DL)
 end
 
 """
 #Testing
-testDay=180
-testLat=45
+testDay=180.0
+testLat=45.00
 testdaylength = daylength(testLat,testDay)
 """
 
-function Initialisation_Clim(yearlength::Float64=365.0,
-    					TempDev::Float64=1.0, TempMax::Float64=30.0, TempMin::Float64=2.0, TempLag::Float64=0.0,
-    					MaxI::Float64=2000.0, MinI::Float64=50.0, IDelay::Float64=0.0,
-    					Latitude::Float64=50.0)
+function Initialisation_Clim(yearlength::Float64,
+    					TempDev::Float64, TempMax::Float64, TempMin::Float64, TempLag::Float64,
+    					MaxI::Float64, MinI::Float64, IDelay::Float64,
+    					Latitude::Float64)
 	Tem = Float64[]
 	Irr = Float64[]
 	Day_length = Float64[]
@@ -44,8 +44,16 @@ function Initialisation_Clim(yearlength::Float64=365.0,
 end
 
 
-"""
+
 #Testing
+Initialisation_Clim(yearlength=365.0, TempDev=1.0, TempMax=30.0, TempMin=2.0, TempLag=0.0,
+    					MaxI=2000.0, MinI=50.0, IDelay=0.0,
+    					Latitude=50.0)
+
+Initialisation_Clim(365.0, 1.0, 30.0, 2.0, 0.0,
+					2000.0, 50.0, 0.0,50.0)
+
+"""
 Temp = Initialisation_Clim()[1]
 Irra = Initialisation_Clim()[2]
 DL = Initialisation_Clim()[3]
