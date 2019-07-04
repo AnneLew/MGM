188073203d936775b724ad0c9b79c3ddf1b4dc6f
AnneLew <45165523+AnneLew@users.noreply.github.com>
2019-07-04 15:36:27 +0200

    Create Model.jl
    new file

---
+++ Model.jl
@@ -0,0 +1,10 @@
+"""
+Model for macrophyte growth, similar to CHARISMA (van Nes 2003)
+"""
+
+include("defaults.jl")
+include("Initialisation.jl")
+include("Resp_PS.jl")
+include("run_simulation.jl")
+
+end
