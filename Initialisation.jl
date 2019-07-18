3de8e0d6ceac728b380268e162363a88ce45d508
AnneLew <45165523+AnneLew@users.noreply.github.com>
2019-07-18 16:54:24 +0200

    Update Initialisation.jl

--- Initialisation.jl
+++ Initialisation.jl
@@ -82,6 +82,7 @@ function getLightD(;irradianceH::Array{Float64}=test_Irr, parFactor::Float64=0.5
                  kdDev::Float64=1.0, maxKd::Float64=2.0, minKd::Float64=2.0, yearlength::Float64=365.0, kdDelay::Float64=-10.0,
                  distWaterSurface::Float64=1.0, plantK::Float64=0.02, higherbiomass::Float64=0.0, fracPeriphyton::Float64=0.2, day::Float64=180.0)
 		 irrSurf = irradianceH * (1 - parFactor) * (1 - fracReflected) * (1 - sunDev) # ÂµE/m^2*s
+		 #lightAttenuCoef: External light attenuation coefficient (extinction coefficient), that is the light attenuation without the effect of vegetation on turbidity [m^-1]
 	     lightAttenuCoef = kdDev * (maxKd - (maxKd-minKd)/2*(2*pi/yearlength)*(day-kdDelay)) #+ Kdisorg + Kparticulates <- TUBRIDITY
 	     lightWater = irrSurf * exp(1)^(- lightAttenuCoef * distWaterSurface - plantK * higherbiomass) # LAMBERT BEER # ÂµE/m^2*s # MÃ¶glichkeit im Exponenten: (absorptivity*c_H2O_pure*dist_water_surface))
 	     lightPlantHour = lightWater - (lightWater * fracPeriphyton) ## ÂµE/m^2*s
