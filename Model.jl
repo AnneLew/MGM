5172e6284982c57a2e73f225ce6769969719347b
AnneLew <45165523+AnneLew@users.noreply.github.com>
2019-07-19 16:28:36 +0200

    Update Model.jl

--- Model.jl
+++ Model.jl
@@ -3,8 +3,13 @@ Model for macrophyte growth, similar to CHARISMA (van Nes 2003)
 """
 
 include("defaults.jl")
+get(defaultSettings(), "lat", "NA")
+
 include("Initialisation.jl")
 include("Resp_PS.jl")
 include("run_simulation.jl")
+
+
+
 
 end
