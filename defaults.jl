0a6876d323a23ff17b88e237f6f397c145aa5cb9
AnneLew <45165523+AnneLew@users.noreply.github.com>
2019-07-26 14:48:01 +0200

    Update defaults.jl

--- defaults.jl
+++ defaults.jl
@@ -10,7 +10,7 @@ function defaultSettings()
     Dict(
 
     #ENVIRONMENTAL VARIABLES
-    "lat" => 47.8, #Latitude of corresponding lake; [°]; 47.8 = Chiemsee
+    "lat" => 80.8, #Latitude of corresponding lake; [°]; 47.8 = Chiemsee
     #"doy" => , #actual day - has to be flexible
     "yearlength" => 365, #Number of days each year [n]; in general on earth
     "tempDev" => 1.0, #share of temp [-]; 1 in CHARISMA
@@ -19,7 +19,7 @@ function defaultSettings()
     "tempLag" => 23, #days after 1st of January where Temp is minimal [d]; 23 in CHARISMA
     "maxI" => 2000.0, #Maximal Irradiance in [µE m^-2 s^-1]; 868 in CHARISMA
     "minI" => 300.0, #Minimal Irradiance [µE m^-2 s^-1]; 96 in CHARISMA
-    "iDelay" => 0.0, #days after 1st of January where I is minimal [d]; -10 in CHARISMA
+    "iDelay" => 0, #days after 1st of January where I is minimal [d]; -10 in CHARISMA
 
     "parFactor" => 0.5, # fraction of total irradiation that is PAR [-]; 0.5 in CHARISMA
     "fracReflected" => 0.1, # light reflection at the water surface [-]; 0.1 in CHARISMA
@@ -31,16 +31,13 @@ function defaultSettings()
     "distWaterSurface" => 1,
     "plantK" => 0.02, #!!SPECIES SPECIFIC!!; [m^2/g]; 0.02 in CHARISMA for C.aspera
     "higherbiomass" => 0.0,
-
     "fracPeriphyton" => 0.2, # !!SPECIES SPECIFIC!!; [-]; 0.2 in CHARISMA for C.aspera
-
     "resp20" => 0.00193, #!!SPECspec!! []; 0.00193 in CHARISMA for C.aspera
     "q10" => 2.0, # !!SPECspec!! []; 2.0 in CHARISMA for C.aspera
     "t1" => 20.0,
 
     #"lightPlantHour" => , #
     "hPhotoLight" => 14.0, #!!SPECspec!!; [µE m^-2 s^-1] ; 14.0 in CHARISMA for C.aspera
-    #"temp" => , #
     "sPhotoTemp" => 1.35,  # !!SPECspec!! []; 1.35 in CHARISMA for C.aspera
     "pPhotoTemp" => 3.0 , # !!SPECspec!! []; 3 in CHARISMA for C.aspera
     "hPhotoTemp" => 14.0, # !!SPECspec!! [°C]; 14.0 in CHARISMA  for C.aspera
@@ -59,9 +56,14 @@ function defaultSettings()
     #"dailyPS" => ,#
     #"dailyRES" => ,#
     "rootShootRatio" => 0.1, #!SPECspec! [-]; 0.1 för C.aspera in CHARISMA
-    "mortalityRate" => 0.0 #
+    "mortalityRate" => 0.0, #
 
+    "growthStart" => 180,  # Spec Spec growth start day
+    "lengthInit" => 0.2, # Spec Spec
+    "weightInit" => 0.1, # Spec Spec
+    "heightMax" => 1.2,  # Spec Spec
 
+    "depthWater" => 2.0
     #SPECIES SPECIFIC VARIABLES
     )
 end
