3bdb0030d252be4d24604dce8ca48eb6605e10b6
AnneLew <45165523+AnneLew@users.noreply.github.com>
2020-06-24 18:57:16 +0200

    new function simulateEnvironment

--- model/output.jl
+++ model/output.jl
@@ -4,7 +4,7 @@ using DelimitedFiles
 using Dates
 
 function writeOutput4Depths(PlantResults1, PlantResults2, PlantResults3, PlantResults4,
-	Temp, Irra, Waterl, lightAttenu, Settings)
+	Env, Settings)
 	homdir = pwd()
 	dirname = Dates.format(now(), "yyyy_m_d_ HH_MM_SS") # "folder" * * string(settings["latitude"])
 	cd(".\\output")
@@ -15,10 +15,10 @@ function writeOutput4Depths(PlantResults1, PlantResults2, PlantResults3, PlantRe
     writedlm( "Plants_3m.csv",  PlantResults3[:,:,1], ',') #Biomass, Number, indWeight, Height, allocatedBiomass, SpreadBiomass
     writedlm( "Plants_4m.csv",  PlantResults4[:,:,1], ',') #Biomass, Number, indWeight, Height, allocatedBiomass, SpreadBiomass
 
-	writedlm( "Temp.csv",  Temp, ',')
-	writedlm( "Irradiance.csv",  Irra, ',')
-	writedlm( "Waterlevel.csv",  Waterl, ',')
-	writedlm( "lightAttenuation.csv",  lightAttenu, ',')
+	writedlm( "Temp.csv",  Env[1], ',')
+	writedlm( "Irradiance.csv",  Env[2], ',')
+	writedlm( "Waterlevel.csv",  Env[3], ',')
+	writedlm( "lightAttenuation.csv",  Env[4], ',')
 
 	writedlm( "Settings.csv",  Settings, ',')
 
